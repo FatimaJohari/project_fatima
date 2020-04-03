7859a102c96803818b8644bf3d3483f45275db47
Fatima <fatemeh.johari@angstrom.uu.se>
2020-04-03 18:08:02 +0200

    new project files

---
+++ forecasting.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 09:55:54 2020
+
+@author: fatjo876
+"""
+
+class 
---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from ... import ...
+
+def main():
+    print("Specify your solar radiation directory")
+
+    forecasting.run()
+
+
+if __name__ == "__main__":
+    main()
